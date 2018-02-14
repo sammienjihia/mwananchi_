fd2695e6874ac98188598a65afff6246811d3070
sammienjihia <dilaninjihia@gmail.com>
2018-02-14 23:05:25 +0300

    No comment

--- Mwananchi/settings.py
+++ Mwananchi/settings.py
@@ -161,7 +161,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'techgravellian@gmail'
 EMAIL_HOST_PASSWORD = 'PsWd@&123#@spoof'
-EMAIL_PORT = '465'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # 587
 
