7ab23f8ecdc6b22c0758871625096431897a1da2
sammienjihia <dilaninjihia@gmail.com>
2018-02-14 23:42:41 +0300

    No comment

--- Mwananchi/settings.py
+++ Mwananchi/settings.py
@@ -162,7 +162,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'techgravellian@gmail.com'
 EMAIL_HOST_PASSWORD = 'PsWd@&123#@spoof'
 EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
+#EMAIL_USE_TLS = True
 # 587
 
 # CELERY STUFF
