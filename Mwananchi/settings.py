8abb3fe7f95762c9d065e40dfea7b4f062bea134
sammienjihia <dilaninjihia@gmail.com>
2018-02-15 00:40:45 +0300

    No comment

--- Mwananchi/settings.py
+++ Mwananchi/settings.py
@@ -159,8 +159,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Email settings
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'techgravellian@gmail.com'
-EMAIL_HOST_PASSWORD = 'PsWd@&123#@spoof'
+EMAIL_HOST_USER = 'mwananchi59@gmail.com'
+EMAIL_HOST_PASSWORD = 'saram2010'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 # 587
